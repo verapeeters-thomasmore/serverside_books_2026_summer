d1751df1d006f8bab74ed5fedadc54b04c03b8b8
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2024-01-17 14:34:40 +0100

    bundel 4: new col Author.description - step 2

--- src/test/java/be/thomasmore/bookserver/controllers/author/AuthorControllerGetOneAuthorTest.java
+++ src/test/java/be/thomasmore/bookserver/controllers/author/AuthorControllerGetOneAuthorTest.java
@@ -21,6 +21,7 @@ public class AuthorControllerGetOneAuthorTest extends AbstractIntegrationTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1))
                 .andExpect(jsonPath("$.name").value("Thomas Mann"))
+                .andExpect(jsonPath("$.description").value("Description Thomas Mann"))
                 .andExpect(jsonPath("$.books").exists())
                 .andExpect(jsonPath("$.books").isEmpty());
     }
