ff2311ca886fa187d2b70b6d214448fce902b76b
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2024-01-17 16:38:09 +0100

    bundel 4: new col Author.country - step 2

--- src/test/resources/sql/authors/create_2_authors.sql
+++ src/test/resources/sql/authors/create_2_authors.sql
@@ -1,5 +1,5 @@
-insert into author (NAME, DESCRIPTION)
-values ('Thomas Mann', 'Description Thomas Mann'); /*1*/
+insert into author (NAME, DESCRIPTION, COUNTRY)
+values ('Thomas Mann', 'Description Thomas Mann', 'Germany'); /*1*/
 
-insert into author (NAME, DESCRIPTION)
-values ('Isaac Asimov', 'Description Isaac Asimov'); /*2*/
+insert into author (NAME, DESCRIPTION, COUNTRY)
+values ('Isaac Asimov', 'Description Isaac Asimov', 'USA'); /*2*/
