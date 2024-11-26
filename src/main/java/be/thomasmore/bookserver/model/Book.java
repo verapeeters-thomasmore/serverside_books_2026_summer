19ec0b429aecfbc3319ca847dd78bf51d8e1f07f
Vera Peeters <vera.peeters@thomasmore.be>
2024-11-26 20:57:12 +0100

    bundel 4: Delete Book.author

--- src/main/java/be/thomasmore/bookserver/model/Book.java
+++ src/main/java/be/thomasmore/bookserver/model/Book.java
@@ -29,10 +29,6 @@ public class Book {
     @Max(value = 200, message = "price should not be greater than 200")
     Integer priceInEur;
 
-    //todo: clean up (with flyway)
-    private String author = ""; //this is not normalized but I don't care for this example
-
-
     @ManyToMany(fetch = FetchType.LAZY)
     private List<Author> authors;
 }
