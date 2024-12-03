ee5c3b38720ae0b8689f3a44399b5cc2f707f292
Vera Peeters <vera.peeters@thomasmore.be>
2024-12-03 17:49:24 +0100

    bundel 5: book.description in GET Request

--- src/test/resources/sql/books/create_2_books.sql
+++ src/test/resources/sql/books/create_2_books.sql
@@ -1,4 +1,4 @@
-insert into book (TITLE)
-values ('Test Automation'); /*1*/
+insert into book (TITLE, DESCRIPTION)
+values ('Test Automation', 'Test Automation Description'); /*1*/
 insert into book (TITLE)
 values ('REST API Automation Testing from Scratch'); /*2*/
