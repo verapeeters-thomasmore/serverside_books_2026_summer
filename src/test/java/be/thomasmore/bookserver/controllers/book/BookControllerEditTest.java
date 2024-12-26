3e589abc5e49d591cf4756335f5cb47b80eef6da
Vera Peeters <vera.peeters@thomasmore.be>
2024-12-26 10:26:54 +0100

    bundel 5: bundel 5: PUT Request voor Book - Unit Test - special case: title is blank

--- src/test/java/be/thomasmore/bookserver/controllers/book/BookControllerEditTest.java
+++ src/test/java/be/thomasmore/bookserver/controllers/book/BookControllerEditTest.java
@@ -4,13 +4,13 @@ import be.thomasmore.bookserver.AbstractIntegrationTest;
 import be.thomasmore.bookserver.model.Book;
 import be.thomasmore.bookserver.model.dto.BookDetailedDTO;
 import be.thomasmore.bookserver.repositories.BookRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,40 +24,42 @@ public class BookControllerEditTest extends AbstractIntegrationTest {
 
     @Test
     @WithMockUser
-    public void editBook() throws Exception {
-        final String BOOK_TITLE = "It is simple to edit a book";
+    public void editBookHappyFlow() throws Exception {
+        final String BOOK_NEW_TITLE = "It is simple to edit a book";
         BookDetailedDTO editBookDto = BookDetailedDTO.builder()
                 .id(1)
-                .title(BOOK_TITLE)
+                .title(BOOK_NEW_TITLE)
                 .build();
 
         mockMvc.perform(getMockRequestPut("/api/books/1", editBookDto))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1))
-                .andExpect(jsonPath("$.title").value(BOOK_TITLE));
+                .andExpect(jsonPath("$.title").value(BOOK_NEW_TITLE));
 
 
-        Book loadedBook = bookRepository.findByTitle(BOOK_TITLE).orElseThrow();
-        assertThat(loadedBook.getTitle()).isEqualTo(BOOK_TITLE);
+        Book loadedBook = bookRepository.findByTitle(BOOK_NEW_TITLE).orElseThrow();
+        assertThat(loadedBook.getTitle()).isEqualTo(BOOK_NEW_TITLE);
     }
 
     @Test
     @WithMockUser
     public void editBookIdNotTheSameAsInDTO() throws Exception {
-        final String BOOK_TITLE = "It is simple to edit a book";
+        final String BOOK1_ORIGINAL_TITLE = "Test Automation";
+        final String BOOK2_ORIGINAL_TITLE = "REST API Automation Testing from Scratch";
+        final String BOOK_NEW_TITLE = "It is simple to edit a book";
         BookDetailedDTO editBookDto = BookDetailedDTO.builder()
                 .id(2)
-                .title(BOOK_TITLE)
+                .title(BOOK_NEW_TITLE)
                 .build();
         mockMvc.perform(getMockRequestPut("/api/books/1", editBookDto))
                 .andExpect(status().isInternalServerError())
                 .andReturn();
 
         Book loadedBook1 = bookRepository.findById(1).orElseThrow();
-        assertThat(loadedBook1.getTitle()).isEqualTo("Test Automation"); // original title
+        assertThat(loadedBook1.getTitle()).isEqualTo(BOOK1_ORIGINAL_TITLE);
 
         Book loadedBook2 = bookRepository.findById(2).orElseThrow();
-        assertThat(loadedBook2.getTitle()).isEqualTo("REST API Automation Testing from Scratch"); // original title
+        assertThat(loadedBook2.getTitle()).isEqualTo(BOOK2_ORIGINAL_TITLE);
     }
 
 
@@ -74,5 +76,17 @@ public class BookControllerEditTest extends AbstractIntegrationTest {
                 .andReturn();
     }
 
+    @Test
+    @WithMockUser
+    public void editBookWithBlankTitle() throws Exception {
+        final String BOOK_ORIGINAL_TITLE = "Test Automation";
+        BookDetailedDTO editBookDto = BookDetailedDTO.builder()
+                .id(1)
+                .title("")
+                .build();
+        Assertions.assertThatThrownBy(() -> mockMvc.perform(getMockRequestPut("/api/books/1", editBookDto)));
 
+        Book loadedBook1 = bookRepository.findById(1).orElseThrow();
+        assertThat(loadedBook1.getTitle()).isEqualTo(BOOK_ORIGINAL_TITLE); // original title
+    }
 }
