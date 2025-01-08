98abe0f3ead2a07982fa259909709c64aea619a9
Vera Peeters <vera.peeters@thomasmore.be>
2025-01-08 17:19:48 +0100

    bundel 6: associations: unit test for PUT /api/books/{ID}/authors

---
+++ src/test/resources/sql/books/create_book_and_authors.sql
@@ -0,0 +1,9 @@
+insert into book (TITLE, DESCRIPTION)
+values ('Test Automation', 'Test Automation Description'); /*1*/
+
+insert into author (NAME, DESCRIPTION, COUNTRY)
+values ('Lasse Koskela', 'Description Lasse Koskela', 'Germany'); /*1*/
+
+insert into author (NAME, DESCRIPTION, COUNTRY)
+values ('Lisa Crispin', 'Description Lisa Crispin', 'USA'); /*1*/
+
