fc1154faef2dd0d46a56ed812394eb0c2851263c
Vera Peeters <vera.peeters@thomasmore.be>
2025-12-15 11:52:40 +0100

    toets 1: entity en data

---
+++ src/main/java/be/thomasmore/bookserver/model/Publisher.java
@@ -0,0 +1,22 @@
+package be.thomasmore.bookserver.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Entity
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String city;
+    private Double baseDiscountPercentage; // Bijvoorbeeld: 3.0 voor 3%
+    private Integer bulkPurchaseQuantity; // Bijvoorbeeld: 500
+    private Double bulkDiscountPercentage; // Bijvoorbeeld: 12.0 voor 12%
+}
